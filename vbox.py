0992971f44f1e0ad44460d105dba17c143855518
Cody Crawford <me@codycrawford.net>
2020-02-10 13:25:06 -0600

    Add minimal bottle service

---
+++ vbox.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import bottle, subprocess
+from bottle import run, get
+
+# TODO: need to add multi-OS "which" to find this
+VBoxManagePath = ['/usr/bin/VBoxManage']
+
+def runVBoxManage(opts):
+    command = VBoxManagePath + opts
+    process = subprocess.check_output(command)
+    return process.splitlines()
+
+@get('/info')
+def getVBoxVersion():
+    our_version = runVBoxManage(['-v'])[0]
+    return {'version': our_version }
+
+if __name__ == "__main__":
+    run(host='localhost', port=8000, debug=True, reloader=True)
